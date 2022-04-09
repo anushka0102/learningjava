8a3db1f743a9d9d3df4c2be41473e8b66d376763
DELL <DELL@DESKTOP-QHGTFA2>
2022-04-09 11:32:17 +0530

    Done till second last

--- FirstJavaProject/src/Unit_01/P3_Task02_OperatorsInJava.java
+++ FirstJavaProject/src/Unit_01/P3_Task02_OperatorsInJava.java
@@ -1,165 +1,197 @@
- package Unit_01;
+ package Unit_01; 
 /*
- * - Variable in java is a data container that saves the values during Java program execution
- *  
- *  Types of Variables:
- *   In Java, there are three types of variables:
- *   
- *     Local Variables- declared inside the body of a method
- *     
- *     Instance Variables- Instance variables are defined without the STATIC keyword.
- *     - They are defined Outside a method declaration.
- *     - They are Object specific are and are known as Instance variables.
- *     
- *      Static Variables-
- *      - static variables are defined with the STATIC keyword
- *      - Static variables are initialized only once, at the start of the program execution.
- *      - These static variables should be initialized first, before, the initialization of any
- *      
- *   - Data Types in Java?
- *   . Reference OneNote
- *   
- *      
+ * operators in java is a symbol that is used to perform operations 
+ * sum=a+b;
+ * 	for example: +,-,*,/
+ * 	#types of operators in java
+ * 		-Unary operators :                    {prefix and postfix}[a++,a--,++a,etc]
+ * 		-Arithmatic operators:                {*,-,/,+,%}
+ * 		-Shift operators:                     {<< , >>}
+ * 		-relational operators:                {> < <= >= !=}
+ * 		-Bitwise operators :                  {& ^ |}
+ * 		-Logical operators :                  {&& ||}
+ * 		-ternary operators:                   {? : }
+ * 		-Assignment operators:                {=,+=,-=,/=,%=,&/,>>=,>>=}
  */
 
-public class P3_Task01_VariablesAndDataTypes {
-	
-	static int q = 11;
-	int p = 10;
+public class P3_Task02_OperatorsInJava {
 
 	public static void main(String[] args) {
 		
-		// Valid Declarations
-		int a, b, c;
-		float pi;
-		double d;
-		char e;
+		OperatorsInJava obj = new OperatorsInJava();
 		
-		// Valid Initializations
-		pi = 3.14f;
-		d = 20.22d;
-		e = 'v';
-		
-		a = 10;
-		b = 10;
-		c = 10;
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		
-		System.out.println(pi);
-		System.out.println(d);
-		System.out.println(e);
-		
-		int myNum = 5; // Integer (whole number)
-		float myFloatNum = 5.99f; // Floating point number
-		char myLetter = 'D'; // Character
-		boolean myBool = true; // Boolean
-		String myText = "Hello"; // String
-		
-		System.out.println(myNum);
-		System.out.println(myFloatNum);
-		System.out.println(myLetter);
-		System.out.println(myBool);
-		System.out.println(myText);
-		
-		/*
-		 * Java Variable Type Conversion & Type Casting: Details are in OneNote
-		 */
-		
-		double f;
-		int i = 10;
-		f = i; // Type Conversion
-		
-		double g = 10;
-		int j;
-		j = (int) g; // Type Casting
-		
-		System.out.println(f);
-		System.out.println(i);
-		System.out.println(g);
-		System.out.println(j);
-		
-		byte k = 10; // 1 byte
-		boolean l = true; // 1 bit = true or false only
-		long m = 10L;
-		float n = 1.2f;
-		double o = 1.2d;
-		
-		System.out.println(k);
-		System.out.println(l);
-		System.out.println(m);
-		System.out.println(n);
-		System.out.println(o);
-		
-		System.out.println(STU.j); //data + function/methods
-		// ABC.display();
-		
-		STU obj1 = new STU();
-		System.out.println(obj1.i++);
-		System.out.println(obj1.i);
-		
-		STU obj2 = new STU();
-		System.out.println(obj2.i);
-		
-		//System.out.println(obj1.j++);
-		//System.out.println(obj1.j);
-		//System.out.println(obj2.j);
-		
-		System.out.println(STU.j++);
-		System.out.println(STU.j);
-		
-		STU.typeconversionAndtypeCasting();
-		
-	}
-	
-	int r = 10;
-	
-	void display() {
-		
-		int a = 5; // local Variable
-		System.out.println("This is Display Method");
-		System.out.println(a);
-		
+		obj.UnaryOperator();
+		obj.ArithmeticOperator();
+		obj.ShiftOperator();
+		obj.RelationalOperator();
+		obj.BitwiseAndLogicalOperator();
+		obj.TernaryOperator();
+		obj.AssignmentOperator();
+
 	}
 
 }
-
-class STU {
-	
-	static int j = 10; // class variable/static variable
-	int i = 10; // Instance Variable
-	
-	static void display() {
-		
-		int a = 5; // local Variable
-		System.out.println("This is Display Method");
-		System.out.println(a);
-		
-	}
-	
-	static void typeconversionAndtypeCasting() {
+class OperatorsInJava
+{
+	void UnaryOperator()  {
+		int a=10; 
+		boolean b = false;
+		System.out.println("Inside the unary Operator");
+		System.out.println(a++);
+		System.out.println(++a);
+		System.out.println(a--);
+		System.out.println(--a);
 		
 		/*
-		 * double f; //8 bytes = 64 bits/slots int i = 10; // 4 bytes = 32 bits/slots f
-		 * = i; //Type Conversion System.out.println(f);
-		 * 
-		 *  double g = 10; //64 int j; //32 j = (int)g;
+		 * Bitwise Complement(~)
+		 * This unary operator returns the one's complement representation of the input value
+		 * with all the bits inverted, which means it makes every 0 to 1, and every 1 to 0.
 		 */
+		System.out.println(~a); //1010 invert all bits ==> 0101 2's complement ==> 1010 + 1 => 1011
+		//000000000000000000000.....1010
 		
-		double f; //64 slots
-		int i = 10; //32 slots
-		f = i; //Type Conversion
-		System.out.println(f);
+		//NOT Operator(!) : It is used to reverse the value of an operand
+		System.out.println(!b); //true
+		System.out.println("\n");
 		
-		double g = 10; //64
-		int j; //32
-		j = (int)g; // Type Casting
 		
-		//32 bits = 64 bits
-		
-		System.out.println(i);
-		System.out.println(j);
 	}
+	void ArithmeticOperator()
+	{
+		System.out.println("Inside Arithmetic Operator");
+		int a=10,b=5;
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	     
+		//What is the output of this expression?
+		System.out.println(((10 * 10) / 5) + 3 - ((1 * 4) / 2));
+		System.out.println("\n");
+	}
+	void ShiftOperator()
+	{
+		System.out.println("Inside Shift Operator");
+		/*
+		 01010101 << 1
+		 10101010
+		 
+		 01010101 >> 1
+		 00101010
+		 
+		 0, 0, 1, 0, 0, 0 {8 >> 1 }
+		 0, 0, 0, 1, 0, 0 {4 >> 1}
+		 0, 0, 0, 0, 1, 0 {2 >> 1}
+		 0, 0, 0, 0, 0, 1 {1}
+		 32, 16, 8, 4, 2, 1
+		 
+		 0, 0, 1, 0, 0, 0 {8 << 1}
+		 0, 1, 0, 0, 0, 0 {16 << 1}
+		 0, 0, 0, 0, 0, 0 {32}
+		 */
+		 
+		System.out.println(10 << 2); // 10 * 2 ^ 2 = 10 * 4 = 40
+		System.out.println(10 >> 2); // 10 * 2 ^ 3 = 10 * 8 = 80
+		System.out.println(20 << 2); // 20 * 2 ^ 2 = 20 * 4 = 80
+		System.out.println(15 << 4); // 15 * 2 ^ 4 = 15 * 16 = 240
+		
+		System.out.println(10 >> 2); // 10 / 2 ^ 2 = 10 / 4 = 2
+		System.out.println(20 >> 2); // 20 / 2 ^ 2 = 20 / 4 = 5
+		System.out.println(20 >> 3); // 20 / 2 ^ 3 = 20 / 8 = 2
+		
+		System.out.println("\n");
+	}
+	// result is true or false
+	
+	void RelationalOperator() {
+		System.out.println("Inside Relational Operator");
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println(a == b);
+		System.out.println(a != b);
+		System.out.println(a > b);
+		System.out.println(a < b);
+		System.out.println(a >= b);
+		System.out.println(a <= b);
+	    
+		System.out.println("\n");
+	}
+	//Bitwise : Second condition is also checked but not in logical && and even in ||
+	
+	void BitwiseAndLogicalOperator()
+	{
+		System.out.println("Inside Bitwise and Logical Operators");
+		var a = 10 ;
+	    int b =  5 ;
+	    int c =  20 ;
+	    //Logical && and Bitwise &
+	    System.out.println(a < b && a++ < c); // f && t = f
+	    System.out.println(a);                //10 because second condition is not checked
+	    
+	    System.out.println(a < b & a++ < c);  // f && t = f
+	    System.out.println(a);                //11 because second condition is checked
+		
+	    //Logical || Bitwise |
+	    System.out.println(a > b || a < c); // t || t = t
+	    System.out.println(a > b | a < c); // t | t = t
+	    
+	    System.out.println(a > b || a++ < c); //t || t = t
+	    System.out.println(a); //10 because second condition is not checked
+	    System.out.println(a > b  | a++ < c); //t \ t = t
+	    System.out.println(a); //11 because second condition is checked
+	    
+	    /*
+	     * Exclusive and Inclusive OR {| and ^}
+	     * In case of or: 0|1 = 1 , 1|0 = 1, 1|1 = 1, 0|0 = 0
+	     * In case of xor: 0|1 = 1, 1|0 = 1, 1|1 = 0, 0|0 = 0
+	     */
+	    System.out.println("Bitwise inclusive OR:" + (12 | 12)); // 1100 | 1100 = 1100
+	    System.out.println("Bitwise exclusive OR:" + (12 ^ 12)); // 1100 ^ 1100 = 0000
+	    
+	    System.out.println("\n");
+	    }
+	    
+	void TernaryOperator() {
+		System.out.println("Inside TernaryOperator");
+		
+		int a = 2;
+		int b = 5;
+		int min = (a < b) ? a : b;
+		System.out.println(min);
+		
+		System.out.println("\n");
+	}
+	
+	void AssignmentOperator() {
+		
+		System.out.println("Inside Assignment Operator");
+		
+		int a = 10;
+		int b = 20;
+		a += 4; // a = a + 4 = 10
+		b -= 4; // b = b - 4 = 16
+		System.out.println(a);
+		System.out.println(b);
+		
+		//Exercise??
+		
+		b >>= 2;
+	    System.out.println(b);
+	    
+	    a = 10;
+	    a += 3;
+	    System.out.println(a);
+	    a -= 4;
+	    System.out.println(a);
+	    a *= 2;
+	    System.out.println(a);
+	    a /= 2;
+	    System.out.println(a);
+	    
+	    System.out.println("\n");
+	}
+	
 }
